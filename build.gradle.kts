cce0c62d7a709933f96de87b29d6cdf9b275cb8d
dougritter <drmachado@gmail.com>
2021-12-09 19:18:37 +0000

    Add web controller to test site with endpoint /

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,7 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("junit:junit:4.13.1")
--- src/main/kotlin/com/iscte/ads/schedulegen/SchedulegenApplication.kt
+++ src/main/kotlin/com/iscte/ads/schedulegen/SchedulegenApplication.kt
@@ -2,23 +2,34 @@ package com.iscte.ads.schedulegen
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
 import java.time.LocalTime
 
+@Controller
 @SpringBootApplication
-class SchedulegenApplication
+class SchedulegenApplication {
+
+	private val roomsGateway = RoomsGatewayImplementation()
+	private val scheduleService = ScheduleGenServiceImplementation(RoomsScheduleGeneratorImplementation(
+			dayStartTime = LocalTime.parse("08:00"),
+			slotGenerator = SlotGeneratorImplementation()
+	))
+	private val scheduleManager = ScheduleGenManagerImplementation(scheduleGenService = scheduleService)
+
+	@RequestMapping("/")
+	@ResponseBody
+	fun generateSchedule(): String? {
+		roomsGateway.loadFile()
+		val result = scheduleManager.generateSchedule(roomsGateway.getRoomsList().toTypedArray(), roomsGateway.getClassesList().toTypedArray())
+		print(result)
+		return result.toString()
+	}
+
+}
 
 fun main(args: Array<String>) {
 	runApplication<SchedulegenApplication>(*args)
 
-	val roomsGateway = RoomsGatewayImplementation()
-	roomsGateway.loadFile()
-
-	val scheduleService = ScheduleGenServiceImplementation(RoomsScheduleGeneratorImplementation(
-			dayStartTime = LocalTime.parse("08:00"),
-			slotGenerator = SlotGeneratorImplementation()
-	))
-	val scheduleManager = ScheduleGenManagerImplementation(scheduleGenService = scheduleService)
-
-	val result = scheduleManager.generateSchedule(roomsGateway.getRoomsList().toTypedArray(), roomsGateway.getClassesList().toTypedArray())
-	print(result)
 }
