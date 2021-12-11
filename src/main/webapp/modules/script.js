09b7817cfb123aa16451ad9c4496676544ba5900
dougritter <drmachado@gmail.com>
2021-12-11 12:45:56 +0000

    Add upload to both rooms and classes csv files

--- src/main/webapp/modules/script.js
+++ src/main/webapp/modules/script.js
@@ -7,42 +7,68 @@ import { stringify } from './csv.js'
 $(document).ready(function() {
     const uploadForm = document.querySelector('.upload')
     uploadForm.addEventListener('submit', function(e) {
-
-        console.log("submitting file")
-
+        console.log("submitting files")
         e.preventDefault()
-        let file = e.target.uploadFile.files[0]
 
-        handleFile(e)
+        handleFiles(e)
     })
-  });
+ });
 
-function handleFile(event) {
-    console.log("submitting file")
-//    var files = event.target.files
-    var file = event.target.uploadFile.files[0]
+function getFileInfo(file) {
+    return `
+         <span style="font-weight:bold;">${escape(file.name)}</span><br>
+         - FileType: ${file.type || 'n/a'}<br>
+         - FileSize: ${file.size} bytes<br>
+         - LastModified: ${file.lastModifiedDate ? file.lastModifiedDate.toLocaleDateString() : 'n/a'}
+        `;
+}
 
-    var fileInfo = `
-      <span style="font-weight:bold;">${escape(file.name)}</span><br>
-      - FileType: ${file.type || 'n/a'}<br>
-      - FileSize: ${file.size} bytes<br>
-      - LastModified: ${file.lastModifiedDate ? file.lastModifiedDate.toLocaleDateString() : 'n/a'}
-    `;
-    $('#file-info').append(fileInfo);
+function handleFiles(event) {
+    console.log("handling files")
+    var roomsFile = event.target.uploadRooms.files[0]
+    var classesFile = event.target.uploadClasses.files[0]
+
+    $('#rooms-file-info').append(getFileInfo(roomsFile))
+    $('#classes-file-info').append(getFileInfo(classesFile))
+
+    var roomsCSV = null
+    var classesCSV = null
 
     var reader = new FileReader();
-    reader.readAsText(file);
+    reader.readAsText(roomsFile);
     reader.onload = function(event){
-      var csv = event.target.result;
+        var csv = event.target.result;
 
-      const parsed = parse(csv)
-      console.log(parsed);
+        const parsed = parse(csv)
+        roomsCSV = parsed
+        console.log(parsed);
 
-      const stringified = stringify(parsed)
+        if (classesCSV != null && roomsCSV != null) {
+            $('#generateSchedule').prop('disabled', false);
+        }
+//      const stringified = stringify(parsed)
 
-      $('#result').empty();
-      $('#result').html(stringified);
-//      $('#result').html(JSON.stringify(data, null, 2));
+//      $('#result').empty();
+//      $('#result').html(stringified);
+    }
+
+    var classesReader = new FileReader();
+        classesReader.readAsText(classesFile);
+        classesReader.onload = function(event){
+            var csv = event.target.result;
+
+            const parsed = parse(csv)
+            classesCSV = parsed
+            console.log(parsed);
+
+            if (classesCSV != null && roomsCSV != null) {
+              $('#generateSchedule').prop('disabled', false);
+            }
+
+//            const stringified = stringify(parsed)
+
+//            $('#result').empty();
+//            $('#result').html(stringified);
     }
 }
 
